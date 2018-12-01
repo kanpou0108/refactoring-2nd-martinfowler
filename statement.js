77e90b14722300aa98a6624fce7795c4eccc05a8
Junpei Ihara <seapig.dolphin2925@gmail.com>
2018-12-02 02:15:18 +0900

    Move volumeCreditsFor

--- statement.js
+++ statement.js
@@ -11,6 +11,7 @@ function statement (invoice, plays) {
     const result = { ...aPerformance };
     result.play = playFor(result);
     result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
 
@@ -39,6 +40,13 @@ function statement (invoice, plays) {
     }
     return result;
   }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
 }
 
 function renderPlainText(data, plays) {
@@ -61,7 +69,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCreditsFor;
     }
     return result;
   }
@@ -70,13 +78,6 @@ function renderPlainText(data, plays) {
     return new Intl.NumberFormat("en-US",
                         { style: "currency", currency: "USD",
                           minimumFractionDigits: 2 }).format(aNumber/100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 }
 
