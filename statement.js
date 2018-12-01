53b55dd5947e12aa7f4f70120d1c62ef2a2ea655
Junpei Ihara <seapig.dolphin2925@gmail.com>
2018-12-01 21:33:08 +0900

    Apply Slide Statements

--- statement.js
+++ statement.js
@@ -7,16 +7,19 @@ const usd = require('./usd')
 
 function statement (invoice) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
+
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
