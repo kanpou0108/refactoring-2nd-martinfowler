b645030a9bb90d702923ede2a16949f310c7ed47
Junpei Ihara <seapig.dolphin2925@gmail.com>
2018-12-01 21:26:15 +0900

    Removing the format Variable

--- statement.js
+++ statement.js
@@ -3,22 +3,21 @@ const invoices = require('./invoices')
 const amountFor = require('./amountFor')
 const playFor = require('./playFor')
 const volumeCreditsFor = require('./volumeCreditsFor')
+const usd = require('./usd')
 
 function statement (invoice) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  const format = new Intl.NumberFormat("en-US",
-                        { style: "currency", currency: "USD",
-                          minimumFractionDigits: 2 }).format;
+
   for (let perf of invoice.performances) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
+  result += `Amount owed is ${usd(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
