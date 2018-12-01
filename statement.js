04e6d490dd9709c9b9cf190ddedbfde29375a1a7
Junpei Ihara <seapig.dolphin2925@gmail.com>
2018-12-01 21:27:04 +0900

    Removing the format Variable. /100 moved

--- statement.js
+++ statement.js
@@ -14,10 +14,10 @@ function statement (invoice) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += `  ${playFor(perf).name}: ${usd(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
-  result += `Amount owed is ${usd(totalAmount/100)}\n`;
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 }
--- usd.js
+++ usd.js
@@ -1,7 +1,7 @@
 function usd(aNumber) {
   return new Intl.NumberFormat("en-US",
                       { style: "currency", currency: "USD",
-                        minimumFractionDigits: 2 }).format(aNumber);
+                        minimumFractionDigits: 2 }).format(aNumber/100);
 }
 
 module.exports = usd
