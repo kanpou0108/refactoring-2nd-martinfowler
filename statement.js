aefdce8e160a93ee78223ee7696f8552986deadd
Junpei Ihara <seapig.dolphin2925@gmail.com>
2018-12-02 02:40:26 +0900

    Create createStatementData

--- statement.js
+++ statement.js
@@ -2,12 +2,16 @@ const plays = require('./plays.json')
 const invoices = require('./invoices')
 
 function statement (invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return renderPlainText(statementData, plays);
+  return renderPlainText(createStatementData(invoice, plays));
+
+  function createStatementData(invoice, plays) {
+    const statementData = {};
+    statementData.customer = invoice.customer;
+    statementData.performances = invoice.performances.map(enrichPerformance);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+    return statementData;
+  }
 
   function enrichPerformance(aPerformance) {
     const result = { ...aPerformance };
