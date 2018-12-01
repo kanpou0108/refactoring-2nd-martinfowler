7bb2c25d166d46d7058f6d7861761ea6ec943a69
Junpei Ihara <seapig.dolphin2925@gmail.com>
2018-12-01 21:14:39 +0900

    volumeCreditsFor

--- statement.js
+++ statement.js
@@ -2,6 +2,7 @@ const plays = require('./plays.json')
 const invoices = require('./invoices')
 const amountFor = require('./amountFor')
 const playFor = require('./playFor')
+const volumeCreditsFor = require('./volumeCreditsFor')
 
 function statement (invoice) {
   let totalAmount = 0;
@@ -11,10 +12,7 @@ function statement (invoice) {
                         { style: "currency", currency: "USD",
                           minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-    // add volume credits
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees
-    if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
     result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
